10c4a869e755adc1ad5130db82dbce9a7cbf89a3
PriyanshuKherade8 <priyanshu.stark2@gmail.com>
2024-09-20 12:44:53 +0530

    feat: right hand side hover and color change

--- src/pages/Collections/Desktop-View/Main.jsx
+++ src/pages/Collections/Desktop-View/Main.jsx
@@ -7,18 +7,22 @@ import {
   Paper,
   Typography,
 } from "@mui/material";
-import React from "react";
-
-// Reusable Card Component
-const ItemCard = ({ image, title }) => (
-  <Card elevation={0}>
+import React, { useState } from "react";
+//   <img src="https://i.ibb.co/NKZD0s8/image-4.jpg" />
+const ItemCard = ({ image, title, isSelected, onClick }) => (
+  <Card elevation={0} onClick={onClick} style={{ marginBottom: "10px" }}>
     <CardActionArea>
       <CardMedia
         component="img"
-        height="180"
-        width="40"
+        height="170"
+        width="30"
         image={image}
         alt={title}
+        style={{
+          border: isSelected ? "1px solid #007AFF" : "none",
+          height: "164px",
+          width: "164px",
+        }}
       />
       <CardContent
         style={{
@@ -32,6 +36,7 @@ const ItemCard = ({ image, title }) => (
             fontSize: "18px",
             fontWeight: "500",
             fontFamily: "Urbanist",
+            color: isSelected ? "#007AFF" : "black",
           }}
         >
           {title}
@@ -58,29 +63,38 @@ const Main = () => {
         "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back07.jpg",
       title: "Item Title3",
     },
-    {
-      image:
-        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back07.jpg",
-      title: "Item Title4",
-    },
   ];
+
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   return (
     <Paper elevation={0} style={{ display: "flex", height: "100vh" }}>
-      <Box style={{ width: "75%", border: "1px solid grey" }}>
+      <Box style={{ width: "84%", border: "1px solid grey" }}>
         {"left screen"}
       </Box>
-      <Box style={{ width: "25%", paddingTop: "4px" }}>
+      <Box style={{ width: "16%", paddingTop: "4px" }}>
         <Paper elevation={3} style={{ padding: "10px", marginBottom: "5px" }}>
           {"Bags"}
         </Paper>
         <Paper
           elevation={3}
-          style={{ padding: "10px", height: "89vh", overflowY: "scroll" }}
+          style={{
+            padding: "10px",
+            height: "89vh",
+            overflowY: "scroll",
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
           <Box>
             {cardItems.map((item, index) => (
-              <ItemCard key={index} image={item.image} title={item.title} />
+              <ItemCard
+                key={index}
+                image={item.image}
+                title={item.title}
+                isSelected={selectedIndex === index}
+                onClick={() => setSelectedIndex(index)}
+              />
             ))}
           </Box>
         </Paper>
