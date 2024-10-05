f4ff3a8b9e821889b908e910fe92aff02bdb525b
PriyanshuKherade8 <priyanshu.stark2@gmail.com>
2024-10-05 16:41:31 +0530

    feat: view ui fix

--- src/pages/Collections/Mobile-View/Components/Views.js
+++ src/pages/Collections/Mobile-View/Components/Views.js
@@ -1,14 +1,15 @@
 import { Box, IconButton, Paper, Tooltip } from "@mui/material";
+import { useState } from "react";
 import { useSetProductChangeCall } from "../../services";
 
 const Views = ({ viewActionData, sessionId, collectionActionData }) => {
-  console.log("viewActionData", viewActionData, collectionActionData);
-
+  const [selectedView, setSelectedView] = useState(null);
   const { mutate: changeViewCall } = useSetProductChangeCall();
 
   const handleViewChange = (view) => {
-    const item = collectionActionData?.items?.[0];
+    setSelectedView(view.view_id);
 
+    const item = collectionActionData?.items?.[0];
     if (!item) {
       console.error("No item found in collectionActionData");
       return;
@@ -27,29 +28,65 @@ const Views = ({ viewActionData, sessionId, collectionActionData }) => {
     };
 
     console.log("Generated viewPayload:", viewPayload);
-
     changeViewCall(viewPayload);
   };
 
   return (
-    <Box style={{ height: "36px" }}>
+    <Box
+      style={{
+        height: "36px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Paper
         elevation={3}
-        sx={{ display: "flex", flexDirection: "row", height: "100%" }}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          borderRadius: "8px",
+          backgroundColor: "#f0f0f0",
+          overflow: "hidden",
+          padding: "1px 2px",
+        }}
       >
-        {/* Map through viewActionData to display the icons */}
         {viewActionData?.map((view) => {
-          // Find the icon with file_type "L"
           const largeIcon = view.view_icons.find(
             (icon) => icon.file_type === "L"
           );
+
           return (
             <Tooltip key={view.view_id} title={view.view_name || view.bag_name}>
-              <IconButton onClick={() => handleViewChange(view)}>
+              <IconButton
+                key={view.view_id}
+                onClick={() => handleViewChange(view)}
+                sx={{
+                  transition: "background-color 0.3s ease",
+                  borderRadius: "8px",
+                  backgroundColor:
+                    selectedView === view.view_id ? "#007AFF" : "transparent",
+                  color: selectedView === view.view_id ? "#fff" : "#000",
+                  "&:hover": {
+                    backgroundColor:
+                      selectedView === view.view_id ? "white" : "#e0e0e0",
+                  },
+                  marginLeft:
+                    view.view_id !== viewActionData[0].view_id ? "0px" : 0,
+                }}
+              >
                 <img
                   src={largeIcon.path}
                   alt={view.view_name || view.bag_name}
-                  style={{ width: 30, height: 30 }}
+                  style={{
+                    width: 20,
+                    height: 20,
+                    filter:
+                      selectedView === view.view_id
+                        ? "none"
+                        : "grayscale(100%)",
+                    transition: "filter 0.3s ease",
+                  }}
                 />
               </IconButton>
             </Tooltip>
